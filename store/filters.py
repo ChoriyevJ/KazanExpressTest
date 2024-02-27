0554ce147bade8ad4e85560618c8e8aed28ac1bd
jamshid <jamshid01072003@gmail.com>
2024-02-27 19:31:47 +0500

    fix: update filters.py

--- store/filters.py
+++ store/filters.py
@@ -6,6 +6,8 @@ class PriceRangeFilterBackend(BaseFilterBackend):
 
     def filter_queryset(self, request, queryset, view):
         query_params = request.query_params
-        if tuple(query_params.keys()) == ('from_price', 'to_price'):
+        from_price = query_params.get('from_price')
+        to_price = query_params.get('to_price')
+        if from_price and to_price:
             return queryset.filter(Q(price__gte=query_params['from_price']) & Q(price__lte=query_params['to_price']))
         return queryset
